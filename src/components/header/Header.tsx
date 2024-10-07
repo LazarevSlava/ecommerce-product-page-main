a6735f203ad44a407641e379ac790ad3ff574263
LazarevSlava <549132w@gmail.com>
2024-10-07 15:14:10 +0300

    modal

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+import Modal from '../modalWindow/ModalWindow';
+
 function Header() {
-    return <></>;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const menuItems = ['Collections', 'Men', 'Women', 'About', 'Contact'];
+
+    return (
+        <>
+            <button onClick={() => setIsModalOpen(true)}>XXX</button>
+            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <ul>
+                    {menuItems.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </Modal>
+        </>
+    );
 }
 export default Header;
---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '*.svg' {
+    import React from 'react';
+    const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    export { ReactComponent };
+    const src: string;
+    export default src;
+  }
+  
