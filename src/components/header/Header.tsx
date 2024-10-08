f2eb39b182aed3e6526edfe5dea9706ad3e8be56
LazarevSlava <549132w@gmail.com>
2024-10-08 13:20:34 +0300

    3

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,8 +9,9 @@ function Header() {
 
     return (
         <div className={style['header-main']}>
-            <div className={style['header-left']}>
+            <div className={style['header-navigation']}>
                 <svg
+                    className={style['menu']}
                     onClick={() => setIsModalOpen(true)}
                     width="16"
                     height="15"
@@ -29,6 +30,12 @@ function Header() {
                         fill-rule="nonzero"
                     />
                 </svg>
+                <div className={style['menu-items']}>
+                    {' '}
+                    {menuItems.map((item) => (
+                        <p className={style['items']}>{item}</p>
+                    ))}
+                </div>
 
                 <Modal
                     isOpen={isModalOpen}
@@ -41,7 +48,7 @@ function Header() {
                     </ul>
                 </Modal>
             </div>
-            <div className={style['header-right']}>
+            <div className={style['header-profile']}>
                 <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
@@ -51,6 +58,7 @@ function Header() {
                 </svg>
                 <img className={style['image']} src={imagePng} alt="My Image" />
             </div>
+            <hr className={style['line']}></hr>
         </div>
     );
 }
