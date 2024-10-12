c975a6e0f0947a6ee27415ec46aed0cd0a5365e2
LazarevSlava <549132w@gmail.com>
2024-10-12 13:18:53 +0300

    svg components

--- src/components/modalWindow/ModalWindow.tsx
+++ src/components/modalWindow/ModalWindow.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './modalWindow.module.scss';
+import CloseIcon from '../icons/CloseIcon';
 
 interface ModalProps {
   isOpen: boolean;
@@ -15,15 +16,8 @@ function ModalWindow({ isOpen, onClose, children }: ModalProps) {
         className={style['modal-content']}
         onClick={(e) => e.stopPropagation()}
       >
-        <button className={style['modal-close']} onClick={onClose}>
-          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-              fill="#69707D"
-              fill-rule="evenodd"
-            />
-          </svg>
-        </button>
+        <CloseIcon className={style['modal-close']} onClick={onClose} />
+
         {children}
       </div>
     </div>
