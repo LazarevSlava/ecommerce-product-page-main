3c9cdb51c025302730f8777a85eace3378b30847
LazarevSlava <549132w@gmail.com>
2024-10-21 14:54:03 +0300

    some new

--- src/components/modalSlider/ModalSlider.tsx
+++ src/components/modalSlider/ModalSlider.tsx
@@ -1,30 +1,11 @@
 import style from './modalSlider.module.scss';
 import CloseIcon from '../icons/CloseIcon';
 import Slider from '../slider/Slider';
-
-import img1Main from '../../assets/images/image-product-1.jpg';
-import img1Thumb from '../../assets/images/image-product-1-thumbnail.jpg';
-import img2Main from '../../assets/images/image-product-2.jpg';
-import img2Thumb from '../../assets/images/image-product-2-thumbnail.jpg';
-import img3Main from '../../assets/images/image-product-3.jpg';
-import img3Thumb from '../../assets/images/image-product-3-thumbnail.jpg';
-import img4Main from '../../assets/images/image-product-4.jpg';
-import img4Thumb from '../../assets/images/image-product-4-thumbnail.jpg';
-
+import { product } from '../../productData/productData';
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
-const product = {
-  id: 1,
-  name: 'Product 1',
-  images: [
-    { main: img1Main, thumbnail: img1Thumb },
-    { main: img2Main, thumbnail: img2Thumb },
-    { main: img3Main, thumbnail: img3Thumb },
-    { main: img4Main, thumbnail: img4Thumb },
-  ],
-};
 
 function ModalSlider({ isOpen, onClose }: ModalProps) {
   if (!isOpen) return null;
--- src/components/viewOfItem/ViewOfItem.tsx
+++ src/components/viewOfItem/ViewOfItem.tsx
@@ -1,26 +1,7 @@
 import AboutItem from '../aboutItem/AboutItem';
 import Slider from '../slider/Slider';
 import style from './viewOfItem.module.scss';
-
-import img1Main from '../../assets/images/image-product-1.jpg';
-import img1Thumb from '../../assets/images/image-product-1-thumbnail.jpg';
-import img2Main from '../../assets/images/image-product-2.jpg';
-import img2Thumb from '../../assets/images/image-product-2-thumbnail.jpg';
-import img3Main from '../../assets/images/image-product-3.jpg';
-import img3Thumb from '../../assets/images/image-product-3-thumbnail.jpg';
-import img4Main from '../../assets/images/image-product-4.jpg';
-import img4Thumb from '../../assets/images/image-product-4-thumbnail.jpg';
-
-const product = {
-  id: 1,
-  name: 'Product 1',
-  images: [
-    { main: img1Main, thumbnail: img1Thumb },
-    { main: img2Main, thumbnail: img2Thumb },
-    { main: img3Main, thumbnail: img3Thumb },
-    { main: img4Main, thumbnail: img4Thumb },
-  ],
-};
+import { product } from '../../productData/productData';
 
 function ViewOfItem() {
   return (
